2757ebd2edd17f5f5f5e3920dae97234feb09f84
olivia.phuonganh@gmail.com <olivia.phuonganh@gmail.com>
2021-05-11 21:06:25 +1000

    we can show, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
   root to: 'contacts#index'
-  
-  devise_for :users
   get 'contacts', to: 'contacts#index', as: 'contacts'
   post 'contacts', to: 'contacts#create'
   get 'contacts/new', to: 'contacts#new', as: 'new_contact'
@@ -11,7 +8,9 @@ Rails.application.routes.draw do
   patch 'contacts/:id', to: 'contacts#update'
   put 'contacts/:id', to: 'contacts#update'
   delete 'contacts/:id', to: 'contacts#destroy'
-
+  
+  devise_for :users
+  get 'users/index'
   get '/restricted', to: 'contacts#restricted', as: 'restricted'
   get '/users', to: 'users#index', as: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
